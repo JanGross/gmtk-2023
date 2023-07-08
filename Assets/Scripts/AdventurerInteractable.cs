a9fbeef0b4649679739afea3effccac03ad4bde1
Minz <git@minzkraut.com>
2023-07-09 00:10:43 +0200

    Implemet quest completion

--- Assets/Scripts/AdventurerInteractable.cs
+++ Assets/Scripts/AdventurerInteractable.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AdventurerInteractable : MonoBehaviour
 {
     public DialogueController dialogueController;
+    public int m_spawnQuestID = 0;
 
     [SerializeField] private string m_name = string.Empty;
     // Start is called before the first frame update
